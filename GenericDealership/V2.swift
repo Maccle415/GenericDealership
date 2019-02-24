cb008e41b585d07723c314c98661d5d57981cdba
MrBlue <dleak22@gmail.com>
2019-02-24 19:52:13 +0200

    V2
    Trying to clean up the first version and make things as generic as possible. I still need to get to gain a deeper understanding of generics in order to do that.

--- GenericDealership/V2.swift
+++ GenericDealership/V2.swift
@@ -8,219 +8,252 @@
 
 import Foundation
 
-///*
-//
-// MODELS
-//
-// */
-//protocol MakeModel {
-//    associatedtype Brand
-//    func make() -> Brand
-//}
-//
-//enum AudiModel: MakeModel {
-//    case a1
-//    case a3
-//
-//    public func make() -> Audi {
-//        switch self {
-//        case .a1:
-//            return Audi(with: .a1)
-//        case .a3:
-//            return Audi(with: .a3)
-//        }
-//    }
-//}
-//
-//enum VWModel: MakeModel {
-//    case golf
-//    case polo
-//
-//    public func make() -> VW {
-//        switch self {
-//        case .golf:
-//            return VW(with: .golf)
-//        case .polo:
-//            return VW(with: .polo)
-//        }
-//    }
-//}
-//
-//
-//
-///*
-//
-// CAR
-//
-// */
-//
-//class Car<Model> {
-//    var model: Model
-//
-//    init(with model: Model) {
-//        self.model = model
-//    }
-//}
-//
-//typealias Audi = Car<AudiModel>
-//typealias VW = Car<VWModel>
-//
-//
-///*
-//
-// FACTORY
-//
-// */
-//
-//struct CarFactoryFactory {
-//    static func makeFactory(with hq: AudiHQ) -> AudiFactory {
-//        return AudiFactory()
-//    }
-//
-//    static func makeFactory(with hq: VWHQ) -> VWFactory {
-//        return VWFactory()
-//    }
-//}
-//
-//protocol CarBuildable {
-//    associatedtype Model
-//
-//    func build(model: Model)
-//}
-//
-//class CarFactory<Brand, Model: MakeModel>: CarBuildable {
-//    private var vehicles: [Brand] = [Brand]() {
-//        didSet {
-//            print(vehicles)
-//        }
-//    }
-//
-//    public func build(model: Model) {
-//        if let vehicle = model.make() as? Brand {
-//            self.vehicles.append(vehicle)
-//        }
-//    }
-//}
-//
-//typealias AudiFactory = CarFactory<Audi, AudiModel>
-//typealias VWFactory = CarFactory<VW, VWModel>
-//
-//
-///*
-//
-// DEALERSHIP
-//
-// */
-//struct DealershipFactory {
-//    static func makeDealership(with hq: AudiHQ) -> AudiDealership {
-//        return AudiDealership(with: hq)
-//    }
-//
-//    static func makeDealership(with hq: VWHQ) -> VWDealership {
-//        return VWDealership(with: hq)
-//    }
-//}
-//
-//protocol Dealership {
-//    associatedtype Model
-//
-//    func placeOrder(for model: Model)
-//    func deliverCar()
-//}
-//
-//class CarDealership<DealershipHQ: CarHQ, Model> {
-//    weak var hq: DealershipHQ?
-//
-//    init(with hq: DealershipHQ) {
-//        self.hq = hq
-//    }
-//}
-//
-//typealias AudiDealership = CarDealership<AudiHQ, AudiModel>
-//extension CarDealership: Dealership where CarDealership == AudiDealership {
-//    func deliverCar() {
-//        print()
-//    }
-//
-//    func placeOrder(for model: AudiModel) {
-//        self.hq?.placeOrder(with: model)
-//    }
-//}
-//
-//typealias VWDealership = CarDealership<VWHQ, VWModel>
-//
-//
-//
-///*
-//
-// HQ
-//
-// */
-//
-//protocol CarHQ: class {
-//    associatedtype HQFactory
-//    associatedtype HQDealership
-//    associatedtype HQModel
-//    associatedtype HQBrand
-//
-//    var factories: [HQFactory] { get set }
-//    var dealerships: [HQDealership] { get set }
-//
-//    func buildFactory()
-//    func buildDealership()
-//
-//    func placeOrder(with model: HQModel)
-//    func deliver(vehicle: HQBrand)
-//}
-//
-//class AudiHQ: CarHQ {
-//    var factories: [AudiFactory] = [AudiFactory]()
-//    var dealerships: [AudiDealership] = [AudiDealership]()
-//
-//    init() {
-//        self.buildFactory()
-//        self.buildDealership()
-//    }
-//
-//    func buildFactory() {
-//        self.factories.append(CarFactoryFactory.makeFactory(with: self))
-//    }
-//
-//    func buildDealership() {
-//        self.dealerships.append(DealershipFactory.makeDealership(with: self))
-//    }
-//
-//    func placeOrder(with model: AudiModel) {
-//        self.factories[0].build(model: model)
-//    }
-//
-//    func deliver(vehicle: Audi) {
-//
-//    }
-//}
-//
-//class VWHQ: CarHQ {
-//    var factories: [VWFactory] = [VWFactory]()
-//    var dealerships: [VWDealership] = [VWDealership]()
-//
-//    init() {
-//        self.buildFactory()
-//        self.buildDealership()
-//    }
-//
-//    func buildFactory() {
-//        self.factories.append(CarFactoryFactory.makeFactory(with: self))
-//    }
-//
-//    func buildDealership() {
-//        self.dealerships.append(DealershipFactory.makeDealership(with: self))
-//    }
-//
-//    func placeOrder(with model: VWModel) {
-//        self.factories[0].build(model: model)
-//    }
-//
-//    func deliver(vehicle: VW) {
-//
-//    }
-//}
+/*
+
+ MODELS
+
+ */
+protocol MakeModel {
+    associatedtype Brand
+    func make() -> Brand
+}
+
+enum AudiModel: MakeModel {
+    case a1
+    case a3
+
+    public func make() -> Audi {
+        switch self {
+        case .a1:
+            return Audi(with: .a1)
+        case .a3:
+            return Audi(with: .a3)
+        }
+    }
+}
+
+enum VWModel: MakeModel {
+    case golf
+    case polo
+
+    public func make() -> VW {
+        switch self {
+        case .golf:
+            return VW(with: .golf)
+        case .polo:
+            return VW(with: .polo)
+        }
+    }
+}
+
+
+
+/*
+
+ CAR
+
+ */
+
+class Car<Model> {
+    var model: Model
+
+    init(with model: Model) {
+        self.model = model
+    }
+}
+
+typealias Audi = Car<AudiModel>
+typealias VW = Car<VWModel>
+
+
+
+/*
+
+ FACTORY
+
+ */
+
+struct CarFactoryFactory {
+    static func makeFactory(with hq: AudiHQ) -> AudiFactory {
+        return AudiFactory(with: hq)
+    }
+
+    static func makeFactory(with hq: VWHQ) -> VWFactory {
+        return VWFactory(with: hq)
+    }
+}
+
+protocol CarBuildable {
+    associatedtype Model
+    associatedtype Brand
+
+    func build(model: Model)
+    func dispatch(vehicle: Brand)
+}
+
+class CarFactory<HQ: CarHQ, Brand, Model: MakeModel>: CarBuildable {
+    weak var delegate: HQ?
+    
+    init(with delegate: HQ) {
+        self.delegate = delegate
+    }
+    
+    private var vehicles: [Brand] = [Brand]() {
+        didSet {
+            if let firstVehicle = self.vehicles.first {
+                self.dispatch(vehicle: firstVehicle)
+            }
+        }
+    }
+
+    public func build(model: Model) {
+        if let vehicle = model.make() as? Brand {
+            self.vehicles.append(vehicle)
+        }
+    }
+    
+    func dispatch(vehicle: Brand) {
+        fatalError("")
+    }
+}
+
+class AudiFactory: CarFactory<AudiHQ, Audi, AudiModel> {
+    override func dispatch(vehicle: Audi) {
+        self.delegate?.deliver(vehicle: vehicle)
+    }
+}
+
+class VWFactory: CarFactory<VWHQ, VW, VWModel> {
+    override func dispatch(vehicle: VW) {
+        self.delegate?.deliver(vehicle: vehicle)
+    }
+}
+
+
+
+/*
+
+ DEALERSHIP
+
+ */
+struct DealershipFactory {
+    static func makeDealership(with hq: AudiHQ) -> AudiDealership {
+        return AudiDealership(with: hq)
+    }
+
+    static func makeDealership(with hq: VWHQ) -> VWDealership {
+        return VWDealership(with: hq)
+    }
+}
+
+protocol Dealership {
+    associatedtype Model
+    associatedtype Brand
+    
+    func placeOrder(for model: Model)
+    func deliverCar(vehicle: Brand)
+}
+
+class CarDealership<DealershipHQ: CarHQ, Model> {
+    weak var hq: DealershipHQ?
+
+    init(with hq: DealershipHQ) {
+        self.hq = hq
+    }
+}
+
+class AudiDealership: CarDealership<AudiHQ, AudiModel>, Dealership {
+    func placeOrder(for model: AudiModel) {
+        self.hq?.placeOrder(with: model)
+    }
+    
+    func deliverCar(vehicle: Audi) {
+        print("Deliver: \(vehicle.model)")
+    }
+}
+
+class VWDealership: CarDealership<VWHQ, VWModel>, Dealership {
+    func placeOrder(for model: VWModel) {
+        self.hq?.placeOrder(with: model)
+    }
+    
+    func deliverCar(vehicle: VW) {
+        print("Deliver: \(vehicle.model)")
+    }
+}
+
+
+
+/*
+
+ HQ
+
+ */
+
+protocol CarHQ: class {
+    associatedtype Factory
+    associatedtype CarDealership
+    associatedtype Model
+    associatedtype Vehicle
+
+    var factories: [Factory] { get set }
+    var dealerships: [CarDealership] { get set }
+
+    func buildFactory()
+    func buildDealership()
+
+    func placeOrder(with model: Model)
+    func deliver(vehicle: Vehicle)
+}
+
+class AudiHQ: CarHQ {
+    var factories: [AudiFactory] = [AudiFactory]()
+    var dealerships: [AudiDealership] = [AudiDealership]()
+
+    init() {
+        self.buildFactory()
+        self.buildDealership()
+    }
+
+    public func buildFactory() {
+        self.factories.append(CarFactoryFactory.makeFactory(with: self))
+    }
+
+    public func buildDealership() {
+        self.dealerships.append(DealershipFactory.makeDealership(with: self))
+    }
+
+    public func placeOrder(with model: AudiModel) {
+        self.factories[0].build(model: model)
+    }
+
+    public func deliver(vehicle: Audi) {
+        self.dealerships[0].deliverCar(vehicle: vehicle)
+    }
+}
+
+class VWHQ: CarHQ {
+    var factories: [VWFactory] = [VWFactory]()
+    var dealerships: [VWDealership] = [VWDealership]()
+
+    init() {
+        self.buildFactory()
+        self.buildDealership()
+    }
+
+    public func buildFactory() {
+        self.factories.append(CarFactoryFactory.makeFactory(with: self))
+    }
+
+    public func buildDealership() {
+        self.dealerships.append(DealershipFactory.makeDealership(with: self))
+    }
+
+    public func placeOrder(with model: VWModel) {
+        self.factories[0].build(model: model)
+    }
+
+    public func deliver(vehicle: VW) {
+        self.dealerships[0].deliverCar(vehicle: vehicle)
+    }
+}
--- GenericDealership/ViewController.swift
+++ GenericDealership/ViewController.swift
@@ -13,174 +13,174 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let hq = AudiHQ()
-        hq.mainDealership()?.order(model: .a1)
+        let audihq = AudiHQ()
+        audihq.placeOrder(with: .a1)
         
         // Do any additional setup after loading the view, typically from a nib.
     }
 }
 
-enum AudiModel {
-    case a1
-    case a3
-}
-
-class Car<Model> {
-    var model: Model
-    
-    init(with model: Model) {
-        self.model = model
-    }
-}
-
-typealias Audi = Car<AudiModel>
-
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-// FACTORY
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-
-protocol Dispatchable {
-    func dispatchVehicle()
-}
-
-class Factory<FactoryHQ: HQ, Stock>: Dispatchable {
-    weak var hqDelegate: FactoryHQ?
-    var stock: [Stock] = [Stock]() {
-        didSet {
-            if !self.stock.isEmpty {
-                self.dispatchVehicle()
-            }
-        }
-    }
-    
-    init(with delegate: FactoryHQ) {
-        self.hqDelegate = delegate
-    }
-    
-    func dispatchVehicle() {
-        fatalError("Subclass to dispatchVehicle")
-    }
-}
-
-class AudiFactory: Factory<AudiHQ, Audi> {
-    override func dispatchVehicle() {
-        if let delegate = self.hqDelegate {
-            delegate.dispatch(vehicle: self.stock.removeFirst())
-        }
-    }
-    
-    func build(model: AudiModel) {
-        switch model {
-        case .a1:
-            self.buildA1()
-        case .a3:
-            self.buildA3()
-        }
-    }
-    
-    private func buildA1() {
-        self.stock.append(Audi(with: .a1))
-    }
-    
-    private func buildA3() {
-        self.stock.append(Audi(with: .a3))
-    }
-}
-
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-// DEALERSHIP
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-
-protocol Orderable {
-    associatedtype Model
-    func order(model: Model)
-}
-
-class Dealership<DealershipHQ: HQ, Model> {
-    weak var hqDelegate: DealershipHQ?
-    
-    init(with delegate: DealershipHQ) {
-        self.hqDelegate = delegate
-    }
-}
-
-typealias AudiDealership = Dealership<AudiHQ, AudiModel>
-extension Dealership: Orderable where Dealership == AudiDealership {
-    func order(model: AudiModel) {
-        if let delegate = self.hqDelegate {
-            delegate.placeOrder(for: model)
-        }
-    }
-}
-
-
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-// HQ
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-
-protocol HQ: class {
-    associatedtype Factory
-    associatedtype Dealership
-    associatedtype Model
-    associatedtype Vehicle
-    
-    var factory: Factory? { get set }
-    var dealerships: [Dealership]? { get set }
-    
-    func mainDealership() -> Dealership?
-    
-    func makeFactory()
-    func makeDealership()
-    
-    func placeOrder(for model: Model)
-    func dispatch(vehicle: Vehicle)
-}
-
-class AudiHQ: HQ  {
-    var factory: AudiFactory?
-    var dealerships: [AudiDealership]?
-    
-    init() {
-        self.setupHQ()
-    }
-    
-    func setupHQ() {
-        self.dealerships = [AudiDealership]()
-        
-        self.makeFactory()
-        self.makeDealership()
-    }
-    
-    func mainDealership() -> AudiDealership? {
-        guard let dealership = self.dealerships?.first else {
-            return nil
-        }
-        
-        return dealership
-    }
-    
-    func makeFactory() {
-        self.factory = AudiFactory(with: self)
-    }
-    
-    func makeDealership() {
-        self.dealerships?.append(AudiDealership(with: self))
-    }
-    
-    func placeOrder(for model: AudiModel) {
-        guard let factory = self.factory else {
-            return
-        }
-        factory.build(model: model)
-    }
-    
-    func dispatch(vehicle: Audi) {
-        print(vehicle.model)
-    }
-}
+//enum AudiModel {
+//    case a1
+//    case a3
+//}
+//
+//class Car<Model> {
+//    var model: Model
+//
+//    init(with model: Model) {
+//        self.model = model
+//    }
+//}
+//
+//typealias Audi = Car<AudiModel>
+//
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+//// FACTORY
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+//
+//protocol Dispatchable {
+//    func dispatchVehicle()
+//}
+//
+//class Factory<FactoryHQ: HQ, Stock>: Dispatchable {
+//    weak var hqDelegate: FactoryHQ?
+//    var stock: [Stock] = [Stock]() {
+//        didSet {
+//            if !self.stock.isEmpty {
+//                self.dispatchVehicle()
+//            }
+//        }
+//    }
+//
+//    init(with delegate: FactoryHQ) {
+//        self.hqDelegate = delegate
+//    }
+//
+//    func dispatchVehicle() {
+//        fatalError("Subclass to dispatchVehicle")
+//    }
+//}
+//
+//class AudiFactory: Factory<AudiHQ, Audi> {
+//    override func dispatchVehicle() {
+//        if let delegate = self.hqDelegate {
+//            delegate.dispatch(vehicle: self.stock.removeFirst())
+//        }
+//    }
+//
+//    func build(model: AudiModel) {
+//        switch model {
+//        case .a1:
+//            self.buildA1()
+//        case .a3:
+//            self.buildA3()
+//        }
+//    }
+//
+//    private func buildA1() {
+//        self.stock.append(Audi(with: .a1))
+//    }
+//
+//    private func buildA3() {
+//        self.stock.append(Audi(with: .a3))
+//    }
+//}
+//
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+//// DEALERSHIP
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+//
+//protocol Orderable {
+//    associatedtype Model
+//    func order(model: Model)
+//}
+//
+//class Dealership<DealershipHQ: HQ, Model> {
+//    weak var hqDelegate: DealershipHQ?
+//
+//    init(with delegate: DealershipHQ) {
+//        self.hqDelegate = delegate
+//    }
+//}
+//
+//typealias AudiDealership = Dealership<AudiHQ, AudiModel>
+//extension Dealership: Orderable where Dealership == AudiDealership {
+//    func order(model: AudiModel) {
+//        if let delegate = self.hqDelegate {
+//            delegate.placeOrder(for: model)
+//        }
+//    }
+//}
+//
+//
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+//// HQ
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+//
+//protocol HQ: class {
+//    associatedtype Factory
+//    associatedtype Dealership
+//    associatedtype Model
+//    associatedtype Vehicle
+//
+//    var factory: Factory? { get set }
+//    var dealerships: [Dealership]? { get set }
+//
+//    func mainDealership() -> Dealership?
+//
+//    func makeFactory()
+//    func makeDealership()
+//
+//    func placeOrder(for model: Model)
+//    func dispatch(vehicle: Vehicle)
+//}
+//
+//class AudiHQ: HQ  {
+//    var factory: AudiFactory?
+//    var dealerships: [AudiDealership]?
+//
+//    init() {
+//        self.setupHQ()
+//    }
+//
+//    func setupHQ() {
+//        self.dealerships = [AudiDealership]()
+//
+//        self.makeFactory()
+//        self.makeDealership()
+//    }
+//
+//    func mainDealership() -> AudiDealership? {
+//        guard let dealership = self.dealerships?.first else {
+//            return nil
+//        }
+//
+//        return dealership
+//    }
+//
+//    func makeFactory() {
+//        self.factory = AudiFactory(with: self)
+//    }
+//
+//    func makeDealership() {
+//        self.dealerships?.append(AudiDealership(with: self))
+//    }
+//
+//    func placeOrder(for model: AudiModel) {
+//        guard let factory = self.factory else {
+//            return
+//        }
+//        factory.build(model: model)
+//    }
+//
+//    func dispatch(vehicle: Audi) {
+//        print(vehicle.model)
+//    }
+//}
